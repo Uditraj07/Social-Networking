4588e781e7a70e7a1223982a3082bcae1f00aebc
unknown <uditrajrout67@gmail.com>
2024-07-07 23:47:55 +0530

    real time message updated using long pooling

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -1,6 +1,7 @@
 const { serUserId, getUserId } = require('../Middleware/auth');
 const User = require('../Models/userModel');
 const Message = require('../Models/messageModel');
+const { Op } = require('sequelize');
 
 exports.addMessage = async (req, res, next) => {
    try {
@@ -26,3 +27,88 @@ exports.addMessage = async (req, res, next) => {
    }
 
 }
+
+exports.allMessages = async (req, res, next) => {
+    try {
+
+        const uname = req.headers['username'];
+        const token = req.cookies.user_id;
+        const userId = getUserId(token);
+
+        const userDetails = await User.findOne({
+            where: { username: uname },
+        });
+        let receiverId = userDetails.dataValues.id;
+
+        const all_messages = await Message.findAll({
+                where: {
+                    [Op.or]: [
+                        {
+                            sender_id: userId,
+                            receiver_id: receiverId
+                        },
+                        {
+                            sender_id: receiverId,
+                            receiver_id: userId
+                        }
+                    ]
+                },
+                include: [
+                    { model: User, as: 'Sender', attributes: ['username','fname','lname'] },
+                    { model: User, as: 'Receiver', attributes: ['username'] }
+                ],
+            attributes: ['id', 'createdAt', 'content'],
+            order: [['createdAt', 'ASC']]
+        });
+
+        res.json({ message: true, all_messages: all_messages });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.newMessage = async (req, res, next) => {
+    try {
+        const username = req.headers['username'];
+        const createdAt = req.headers['createdat'];
+
+        if (!username || !createdAt) {
+            return res.status(400).json({ message: false, error: 'Missing headers' });
+        }
+
+        const userDetails = await User.findOne({
+            where: { username: username },
+        });
+
+        if (!userDetails) {
+            return res.status(404).json({ message: false, error: 'User not found' });
+        }
+
+        let receiverId = userDetails.dataValues.id;
+
+        const newMessage = await Message.findOne({
+            where: {
+                sender_id: receiverId,
+                createdAt: {
+                    [Op.gt]: createdAt
+                }
+            },
+            include: [
+                { model: User, as: 'Sender', attributes: ['username','fname','lname'] },
+                { model: User, as: 'Receiver', attributes: ['username'] }
+            ],
+            order: [['createdAt', 'ASC']],
+            attributes: ['id', 'createdAt', 'content']
+        });
+        
+
+        if (!newMessage) {
+            return res.json({ message: true, new_message: null });
+        }
+
+        res.json({ message: true, new_message: newMessage });
+    } catch (error) {
+        console.error('Error fetching new message:', error);
+        res.status(500).json({ message: false, error: 'Internal server error' });
+    }
+};
--- Routes/messageRoutes.js
+++ Routes/messageRoutes.js
@@ -3,6 +3,8 @@ const router = Router();
 const messageController = require('../Controllers/messageController');
 const { userAuth } = require('../Filters/userAuth');
 
-router.post('/add-message',userAuth, messageController.addMessage);
+router.post('/add-message', userAuth, messageController.addMessage);
+router.get('/get-all-messages', userAuth, messageController.allMessages);
+router.get('/new-message', userAuth, messageController.newMessage);
 
 module.exports = router;
